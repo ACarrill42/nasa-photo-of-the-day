407e0194eb161083f38ac0fd48f518c512346892
ACarrill42 <austincarrill@gmail.com>
2021-02-02 21:26:24 -0600

    finished state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+import {BASE_URL , API_KEY} from './API';
 
 function App() {
+
+  const [info, setInfo] = useState([]);
+
+  const fetchNasa = () => {
+    axios.get(`${BASE_URL}/?api_key=${API_KEY}`)
+    .then(res => {
+      console.log(res.data)
+      setInfo(res.data);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+    useEffect(fetchNasa, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1>NASA API!!!</h1>
+      <h2>Title: {info.title}</h2>
+      <p>Information: {info.explanation}</p>
+      <p>Date: {info.date}</p>
+      <img src ={info.url} alt ={info.title}/>
+      <p>Copyright: {info.copyright}</p>
     </div>
   );
 }
+
+// function Body(props) {
+//   const {info , setInfo} = props;
+
+
+// return (
+
+//   <div>{props.url}</div>
+
+//   );
+// }
 
 export default App;
