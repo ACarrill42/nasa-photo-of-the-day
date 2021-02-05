0b5a058799a725b8aa4006e92af6f59b376e8af6
ACarrill42 <austincarrill@gmail.com>
2021-02-04 20:15:28 -0600

    working on props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import {BASE_URL , API_KEY} from './API';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div `
+  color: purple;
+  background: skyblue;
+
+  h1 {
+    color: red;
+  }
+  h2 {
+    color: green;
+  }
+
+  ${props => (props.type === 'info' ? 'color: orange;' : null)}
+  ${props => (props.type === 'date' ? 'color: gold;' : null)}
+  ${props => (props.type === 'copyright' ? 'color: magenta;' : null)}
+`;
 
 function App() {
 
@@ -21,14 +38,14 @@ function App() {
     useEffect(fetchNasa, [])
 
   return (
-    <div className="App">
+    <StyledDiv className="App">
       <h1>NASA API!!!</h1>
       <h2>Title: {info.title}</h2>
-      <p>Information: {info.explanation}</p>
-      <p>Date: {info.date}</p>
+      <p type='info'>Information: {info.explanation}</p>
+      <p type='date'>Date: {info.date}</p>
       <img src ={info.url} alt ={info.title}/>
-      <p>Copyright: {info.copyright}</p>
-    </div>
+      <p type='copyright'>Copyright: {info.copyright}</p>
+    </StyledDiv>
   );
 }
 
